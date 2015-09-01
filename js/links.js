534b2b2be9bf5f52a42034122f2970096f8be17e
maxwad <maxwad@yandex.ru>
2015-09-01 15:44:47 +0300

    second commit

--- js/links.js
+++ js/links.js
@@ -1,6 +1,22 @@
 $(document).ready(function () {
 	
-	$(".slider_next").click(function (e) {
+	$(".activate").click(function (e) {
+        $(".activate").toggleClass("deactivate");
+        var link = $(".link");
+        var i = 0;
+
+        if ($(link[i]).hasClass("down")){
+            var down = setInterval(function(){
+                if(i>=link.length) {
+                    clearInterval(down);
+                }
+                $(link[i]).removeClass("down");
+                i++;
+            },50);
+        }
+        else {
+            $(link).addClass("down");
+        }
 
 	});
 
