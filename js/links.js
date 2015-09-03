2fbe5240cd22c724605defdeb3f319fd4eb14504
maxwad <maxwad@yandex.ru>
2015-09-03 10:49:07 +0300

    final

--- js/links.js
+++ js/links.js
@@ -1,10 +1,11 @@
 $(document).ready(function () {
-	
-	$(".activate").click(function (e) {
-        $(".activate").toggleClass("deactivate");
-        var link = $(".link");
-        var i = 0;
 
+    var link = $(".link");
+
+	$(".activate").click(function () {
+        $(".activate").toggleClass("deactivate");
+        $(".links_block .wrapper").toggleClass("zoom");
+        var i = 0;
         if ($(link[i]).hasClass("down")){
             var down = setInterval(function(){
                 if(i>=link.length) {
@@ -15,10 +16,16 @@ $(document).ready(function () {
             },50);
         }
         else {
+            $(link).removeClass("open_link");
             $(link).addClass("down");
+            $(".link+.activate").removeClass("active_down");
         }
-
 	});
+
+    $(link).click(function () {
+        $(this).addClass("open_link");
+        $(".link+.activate").addClass("active_down");
+    });
 
 
 	
